0b7fe23cda63369a7a0231ab1a0713c25d59faf4
github Ryuya-web <mofa196@mbox.re>
2021-04-14 09:13:26 +0900

    fix

--- import.rb
+++ import.rb
@@ -1,23 +1,26 @@
 require "csv"
 require 'pry'
 result = CSV.read('sample5.csv', headers: true).map(&:to_hash)
-book_array = []
+@book_array = []
 result.each do |one|
   arr = []
   book1 = one['本①のタイトル']
   book2 = one['本②のタイトル']
   book3 = one['本③のタイトル']
-  if !book1.nil? && !book_array.include?(one['本①のタイトル'])
-    arr.push(one['名前'].concat(':'),one['本①のタイトル'],one['本①の著者名'],one['本①の出版年'])
-    book_array.push(one['本①のタイトル'],one['本②のタイトル'],one['本③のタイトル'])
+  def book_add(a,b,c)
+    @book_array.push(a,b,c)
+  end
+  if !book1.nil? && !@book_array.include?(book1)
+    arr.push(one['名前'].concat(':'),book1,one['本①の著者名'],one['本①の出版年'])
+    book_add(book1,book2,book3)
     puts arr.join('　')
-  elsif !book2.nil? && !book_array.include?(one['本②のタイトル'])
-    arr.push(one['名前'].concat(':'),one['本②のタイトル'],one['本②の著者名'],one['本②の出版年'])
-    book_array.push(one['本①のタイトル'],one['本②のタイトル'],one['本③のタイトル'])
+  elsif !book2.nil? && !@book_array.include?(book2)
+    arr.push(one['名前'].concat(':'),book2,one['本②の著者名'],one['本②の出版年'])
+    book_add(book1,book2,book3)
     puts arr.join('　')
-  elsif !book3.nil? || !book_array.include?(one['本③のタイトル'])
-    arr.push(one['名前'].concat(':'),one['本③のタイトル'],one['本③の著者名'],one['本③の出版年'])
-    book_array.push(one['本①のタイトル'],one['本②のタイトル'],one['本③のタイトル'])
+  elsif !book3.nil? || !@book_array.include?(book3)
+    arr.push(one['名前'].concat(':'),book3,one['本③の著者名'],one['本③の出版年'])
+    book_add(book1,book2,book3)
     puts arr.join('　')
   else
     p "本が存在しません"
